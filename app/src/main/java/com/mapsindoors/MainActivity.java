b2716aee92819ace897bc9c5f9476562f51c98f2
Jose J Varo <jjv@mapspeople.com>
2018-03-18 21:05:25 +0100

    Updated project to our v2 SDK, added several extras

--- app/src/main/java/com/mapsindoors/MainActivity.java
+++ app/src/main/java/com/mapsindoors/MainActivity.java
@@ -7,7 +7,6 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.mapspeople.Location;
 import com.mapspeople.LocationDisplayRule;
 import com.mapspeople.MapControl;
@@ -15,9 +14,12 @@ import com.mapspeople.MapsIndoors;
 import com.mapspeople.dbglog;
 import com.mapspeople.errors.MIError;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity
+{
     public static final String TAG = MainActivity.class.getSimpleName();
 
     MapControl myMapControl;
@@ -32,13 +34,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // MapsIndoors SDK debug setup
-        {
-            // Enable/disable internal debugging ASAP
-            dbglog.useDebug( BuildConfig.DEBUG  );
-
-            // Add a log tag prefix to the MI SDK logs
-            dbglog.setCustomTagPrefix( BuildConfig.FLAVOR + "_" );
-        }
+//        {
+//            // Enable/disable internal debug messages / assertions
+//            dbglog.useDebug( BuildConfig.DEBUG );
+//
+//            // Add a log tag prefix to the MI SDK logs
+//            dbglog.setCustomTagPrefix( BuildConfig.FLAVOR + "_" );
+//        }
 
         // Initialize MapsIndoors Here
         MapsIndoors.initialize(
@@ -58,53 +60,76 @@ public class MainActivity extends AppCompatActivity {
         //
         mapFragment = ((SupportMapFragment) getSupportFragmentManager().findFragmentById( R.id.map_fragment ));
 
+        //
         mapFragment.getMapAsync( googleMap -> {
 
             mMap = googleMap;
-            mMap.moveCamera( CameraUpdateFactory.newLatLngZoom( new LatLng( 50.8591041, 4.4508542 ), 19.5f ) );
 
-            setupMapIfNeeded();
+            //
+            mMap.moveCamera( CameraUpdateFactory.newLatLngZoom( new LatLng( 57.05813067, 9.95058065 ), 13.0f ) );
+
+            setupMapsIndoors();
         } );
     }
 
-    void setupMapIfNeeded() {
+    void setupMapsIndoors() {
 
         myMapControl = new MapControl(this, mapFragment, mMap);
 
         //
-//        LocationDisplayRules displayRules = new LocationDisplayRules();
-//        final LocationDisplayRule ruleA = new LocationDisplayRule.Builder("Office").setBitmapIcon(R.drawable.misdk_dot_black).setShowLabel(false).setZOn(17).build();
+//        {
+//            LocationDisplayRules displayRules = new LocationDisplayRules();
+//            final LocationDisplayRule ruleA = new LocationDisplayRule.Builder("Office").
+//                    setBitmapIcon( R.drawable.misdk_dot_black ).
+//                    setShowLabel( false ).
+//                    setZOn( 17 ).
+//                    build();
 //
-//        displayRules.add(ruleA);
-//        myMapControl.addDisplayRules( displayRules);
+//            displayRules.add( ruleA );
+//            myMapControl.addDisplayRules( displayRules );
+//        }
+//        {
+//            final LocationDisplayRule ruleA = new LocationDisplayRule.Builder( "Office" ).
+//                    setBitmapIcon( R.drawable.misdk_dot_black ).
+//                    setShowLabel( false ).
+//                    setZOn( 17 ).
+//                    build();
+//
+//            myMapControl.getDisplayRules().add( ruleA );
+//        }
 
+        myMapControl.setOnMarkerClickListener( marker -> {
 
-        myMapControl.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-
-                Location aLocation = myMapControl.getLocation( marker );
-                if(aLocation!=null)
-                {
-                    String aa = aLocation.getId();
-                    if(BuildConfig.DEBUG){}
-                }
-                return true;
+            Location aLocation = myMapControl.getLocation( marker );
+            if(aLocation!=null)
+            {
+                //String locationName = aLocation.getName();
             }
-        });
+
+            return true;
+        } );
 
         myMapControl.init( errorCode -> {
             if( errorCode == MIError.NO_ERROR )
             {
-                runOnUiThread( new Runnable()
-                {
-                    @Override
-                    public void run()
-                    {
+                runOnUiThread( () -> {
 
-                    }
+                    //
+                    myMapControl.selectFloor( 1 );
+
+                    //
+                    mMap.animateCamera( CameraUpdateFactory.newLatLngZoom( new LatLng( 57.05813067, 9.95058065 ), 19f ) );
                 });
             }
         });
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        // For now, terminate the SDK when the activity is destroyed
+        MapsIndoors.onApplicationTerminate();
+
+        super.onDestroy();
     }
 }
